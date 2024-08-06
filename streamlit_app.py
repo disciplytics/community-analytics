d37f6ba0faa5ad15fe2945dd57186c7a3fba5ae3
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 10:23:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,27 @@ import streamlit as st
 
 st.title('Community Analytics')
 st.subheader('An app to better understand your neighbors...')
+
+
+
+# demographics pages
+population = st.Page(
+  'demographics/population.py', title = 'Population Report', icon=":material/groups:", default=True
+)
+
+race_ethnicity = st.Page(
+  'demographics/race_ethnicity.py', title = 'Race and Ethnicity Report', icon=":material/diversity_1:", default=True
+)
+
+age = st.Page(
+  'demographics/age.py', title = 'Age Report', icon=":material/flare:", default=True
+)
+
+# socioeconomics pages
+income = st.Page(
+  'socioeconomics/income.py', title = 'Income Report', icon=":material/payments:", default=True
+)
+
+poverty = st.Page(
+  'socioeconomics/poverty.py', title = 'Poverty Report', icon=":material/money_off:", default=True
+)
