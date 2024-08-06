f84123919eba701bbebd68f4003eb72beaf4950f
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 12:56:41 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -3,12 +3,16 @@ import streamlit as st
 st.title('Community Analytics')
 st.subheader('An app to better understand your neighbors...')
 
- 
+
+# connect to snowflake
 conn = st.connection("snowflake")
 
+# get the list of CBSAs for the user
 cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES ORDER BY GEO_NAME ASC', ttl=0)
 
+# return list of CBSA in a select box
 cbsa_selection = st.selectbox(
- 'Select a Metro/Micropolitan Area (Type to Search)',cbsa_list)
+ 'Select a Metro/Micropolitan Area (Type a city name to search)',cbsa_list)
 
-st.write(cbsa_selection)
+
+st.write(f'Please enjoy the community report for the {cbsa_selection}')
