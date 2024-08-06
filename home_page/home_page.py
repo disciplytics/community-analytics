ebc014a398e21ae6ac11d0786a57faa2f11d5710
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 12:49:27 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -9,6 +9,6 @@ conn = st.connection("snowflake")
 cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES', ttl=0)
 
 cbsa_selection = st.selectbox(
- 'Select a Metro/Micropolitan Area',cbsa_list)
+ 'Select a Metro/Micropolitan Area (Type to Search)',cbsa_list)
 
 st.write(cbsa_selection)
