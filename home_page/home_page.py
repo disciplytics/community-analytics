7d3865756a9fd268d6ecc99ebd3a7728dd22027d
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 10:52:13 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title('Community Analytics')
 st.subheader('An app to better understand your neighbors...')
+
+
+conn = st.connection('snowflake')
+
+cbsa_list = conn.query('SELECT DISTINCT GEO_NAME as "Metro/Micro Area" FROM CBSA_DATA')
+
+st.write(cbsa_list)         
