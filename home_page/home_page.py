68b19638d41ccaab00e337f627037524a26138c1
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 12:47:25 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -6,6 +6,11 @@ st.subheader('An app to better understand your neighbors...')
  
 conn = st.connection("snowflake")
 
-cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES', ttl=0)
+cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES', ttl=0).values
 
-st.write(cbsa_list)         
+cbsa_selection = st.selectbox(
+ label = 'Select a Metro/Micropolitan Area',
+ cbsa_list
+)
+
+st.write(cbsa_selection)
