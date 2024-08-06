c8a57ba351c9283d2cc517ed0182eca4f9aa2266
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 11:59:01 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -5,6 +5,6 @@ st.subheader('An app to better understand your neighbors...')
 
 conn = st.connnection("snowflake")
 
-cbsa_list = conn.query('SELECT DISTINCT GEO_NAME FROM CBSA_DATA LIMIT 1000;', ttl=600)
+cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES;', ttl=600)
 
 st.write(cbsa_list)         
