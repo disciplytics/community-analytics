f2a2ad800f8659a97ba9ecf412062edafcaa1495
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 12:26:43 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -4,7 +4,7 @@ st.title('Community Analytics')
 st.subheader('An app to better understand your neighbors...')
 
  
-conn = st.connection("default", type = 'snowflake')
+conn = st.connection("snowflake", type = 'snowflake')
 
 cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES;', ttl=0)
 
