392511d2cd7c8601d5a61bc93d949209f067b4b7
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 10:43:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,29 @@
 import streamlit as st
 
-home_page = st.Page()
+home_page = st.Page(
+  'home_page/home_page.py', title = 'Home', icon=":material/home:", default=True
+)
 
 # demographics pages
 population = st.Page(
-  'demographics/population.py', title = 'Population Report', icon=":material/groups:", default=True
+  'demographics/population.py', title = 'Population Report', icon=":material/groups:"
 )
 
 race_ethnicity = st.Page(
-  'demographics/race_ethnicity.py', title = 'Race and Ethnicity Report', icon=":material/diversity_1:", default=True
+  'demographics/race_ethnicity.py', title = 'Race and Ethnicity Report', icon=":material/diversity_1:"
 )
 
 age = st.Page(
-  'demographics/age.py', title = 'Age Report', icon=":material/flare:", default=True
+  'demographics/age.py', title = 'Age Report', icon=":material/flare:"
 )
 
 # socioeconomics pages
 income = st.Page(
-  'socioeconomics/income.py', title = 'Income Report', icon=":material/payments:", default=True
+  'socioeconomics/income.py', title = 'Income Report', icon=":material/payments:"
 )
 
 poverty = st.Page(
-  'socioeconomics/poverty.py', title = 'Poverty Report', icon=":material/money_off:", default=True
+  'socioeconomics/poverty.py', title = 'Poverty Report', icon=":material/money_off:"
 )
 
 
