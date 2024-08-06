1ba0d0d806726204d972b8f8785d184d7b5cbcae
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-06 12:06:02 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title('Community Analytics')
 st.subheader('An app to better understand your neighbors...')
 
+ 
 conn = st.connection("snowflake", type = "snowflake")
 
 cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES;', ttl=600)
