58559b4f612edba8dbbfe90abf3c3822cc58e7d4
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 12:05:47 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -24,4 +24,28 @@ st.write(f'Please enjoy the community report for the {cbsa_selection}. See the v
 geojson_sql = "SELECT COORDS FROM CBSA_DATA WHERE GEO_NAME = " + f"'{cbsa_selection}'" + "LIMIT 1"
 geojson = conn.query(geojson_sql, ttl=0)
 
-st.write(ast.literal_eval(geojson['COORDS'][0]))
+# covert to dictionary
+geojson_dict = ast.literal_eval(geojson['COORDS'][0])
+
+# create the GeoJson layer
+geojson_layer = pdk.Layer(
+    "GeoJsonLayer",
+    geojson_dict,
+    opacity=0.8,
+    stroked=False,
+    filled=True,
+    extruded=True,
+    wireframe=True,
+    get_elevation="20",
+    get_fill_color="[255, 255, 255]",
+    get_line_color=[255, 255, 255],
+)
+
+INITIAL_VIEW_STATE = pdk.ViewState(latitude=geojson_dict['coordinates'][0][0][1], longitude=geojson_dict['coordinates'][0][0][0], zoom=10.5, max_zoom=16, pitch=45, bearing=0)
+
+# create the pydeck using the geojson layer
+r = pdk.Deck(layers=[ geojson_layer ], map_style=None, initial_view_state=INITIAL_VIEW_STATE)
+
+# display the pydeck
+st.pydeck_chart(r)
+
