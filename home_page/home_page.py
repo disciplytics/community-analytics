248332e4fb1b843f13c96f3f34700a73c4225edf
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 09:37:19 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -17,6 +17,7 @@ cbsa_selection = st.selectbox(
 st.write(f'Please enjoy the community report for the {cbsa_selection}. See the views on the left.')
 
 # get geojson for selected area
-geojson = conn.query(f"SELECT GEO_JSON FROM CBSA_DATA WHERE GEO_NAME = {cbsa_selection} LIMIT 1;", ttl=0)
+geojson_sql = f"SELECT GEO_JSON FROM CBSA_DATA WHERE GEO_NAME = {cbsa_selection} LIMIT 1"
+geojson = conn.query(geojson_sql, ttl=0)
 
 st.write(geojson)
