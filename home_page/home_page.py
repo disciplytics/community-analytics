03f455271da7bb05186377ce538233953483953e
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 10:52:10 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -1,5 +1,8 @@
 import streamlit as st
-import json
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
 st.title('Community Analytics')
 st.subheader('An app to better understand your neighbors...')
 
@@ -20,3 +23,33 @@ st.write(f'Please enjoy the community report for the {cbsa_selection}. See the v
 polygon_sql = "SELECT POLYGON FROM CBSA_DATA WHERE GEO_NAME = " + f"'{cbsa_selection}'" + "LIMIT 1"
 polygon = conn.query(polygon_sql, ttl=0)
 st.write(polygon.POLYGON[0])
+
+polygon_layer_snow = pdk.Layer(
+        "PolygonLayer",
+        polygon,
+        #id="geojson",
+        opacity=0.05,
+        stroked=True,
+        get_polygon="POLYGON",
+        filled=True,
+        get_line_color=[200, 200, 200],
+        auto_highlight=True,
+        pickable=True,
+    )
+
+
+r = pdk.Deck(
+        map_style=None,
+        #initial_view_state=pdk.ViewState(
+        #latitude=center_point[1],
+        #longitude=center_point[0],
+        #zoom=3,
+        #pitch=50,
+    #),
+    layers=[my_polygons]    
+)
+
+st.pydeck_chart(r)
+
+
+
