6dc30de743aea1e5aaefa1bf4096bf851094d970
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 13:21:00 -0400

    Update population.py

--- demographics/population.py
+++ demographics/population.py
@@ -11,10 +11,18 @@ conn = st.connection("snowflake")
 
 # total pop etl
 total_pop_sql = "SELECT GEO_NAME, VARIABLE_NAME as Race, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE VARIABLE_NAME = 'Race: Population | Total, 5yr Estimate' AND GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
-#total_pop_sql = "SELECT GEO_NAME, VARIABLE_NAME as Legend, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
-
 # get the total population
 total_pop_df = conn.query(total_pop_sql, ttl=0)
+
+# total HH etl
+total_hh_sql = "SELECT  GEO_NAME, VARIABLE_NAME as HOUSEHOLDS, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_HHFAM_DATA WHERE VARIABLE_NAME = 'Household Type By Household Size: Population | Total, 5yr Estimate' AND GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
+# get the total number of households
+total_hh_df = conn.query(total_hh_sql, ttl=0)
+
+# total familes etl
+total_fam_sql = "SELECT GEO_NAME, VARIABLE_NAME as FAMILIES, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_HHFAM_DATA WHERE VARIABLE_NAME = 'Household Type By Household Size: Population | Family households, 5yr Estimate' AND GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
+# get the total number of familes
+total_fam_df = conn.query(total_fam_sql, ttl=0)
 
 st.subheader('Population Trends')
 st.line_chart(
@@ -23,3 +31,19 @@ st.line_chart(
   y = 'FIVE_YEAR_ESTIMATE',
   x_label = '5 Year Estimate Date',
   y_label = 'Population Count')
+
+st.subheader('Household Trends')
+st.line_chart(
+  data = total_hh_df, 
+  x = 'FIVE_YEAR_ESTIMATE_DATE', 
+  y = 'FIVE_YEAR_ESTIMATE',
+  x_label = '5 Year Estimate Date',
+  y_label = 'Household Count')
+
+st.subheader('Family Trends')
+st.line_chart(
+  data = total_fam_df, 
+  x = 'FIVE_YEAR_ESTIMATE_DATE', 
+  y = 'FIVE_YEAR_ESTIMATE',
+  x_label = '5 Year Estimate Date',
+  y_label = 'Family Count')
