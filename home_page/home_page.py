28e664182cf23f989be3e9e1f68b3e658919864a
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 11:59:15 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -20,34 +20,6 @@ cbsa_selection = st.selectbox(
 
 st.write(f'Please enjoy the community report for the {cbsa_selection}. See the views on the left.')
 
-# get polygon for selected area
-polygon_sql = "SELECT POLYGON FROM CBSA_DATA WHERE GEO_NAME = " + f"'{cbsa_selection}'" + "LIMIT 1"
-polygon = conn.query(polygon_sql, ttl=0)
-
-#st.write(np.array(polygon['POLYGON'][0][9:-2]))
-
-# This array will contain your polygons for each district
-polygons = []
-
-# Iterate through the response records
-for record in polygon["POLYGON"]:
-    # This array will contain coordinates to draw a polygon
-    coordinates = []
-    
-    # Iterate through the coordinates of the record
-    for coord in record["fields"]["geo_shape"]["coordinates"][0]:
-        lon = coord[0] # Longitude
-        lat = coord[1] # Latitude
-        
-        # /!\ Order of lon & lat might be wrong here
-        coordinates.append((lon, lat))
-
-    # Append a new Polygon object to the polygons array
-    # (Note that there are outer brackets, I'm not sure if you can
-    # store all polygons in a single Polygon object)
-    polygons.append(Polygon([coordinates]))
-
-st.write(polygons)
-
-
-
+# get geojson for selected area
+geojson_sql = "SELECT COORDS FROM CBSA_DATA WHERE GEO_NAME = " + f"'{cbsa_selection}'" + "LIMIT 1"
+geojson = conn.query(geojson_sql, ttl=0)
