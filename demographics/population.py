8216d0b7537fc39f7440451ac215011c97b9cd83
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 12:28:54 -0400

    Update population.py

--- demographics/population.py
+++ demographics/population.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title('Population Report')
 
 
+# connect to snowflake
+conn = st.connection("snowflake")
+
+'''SELECT 
+    GEO_NAME,
+    VARIABLE_NAME as Race,
+    DATE as Five_Year_Estimate_Date,
+    VALUE as Five_Year_Estimate
+FROM COMMUNITY_ANALYTICS.PUBLIC.CBSA_RACE_DATA'''
+
+# get the total population
+cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_RACE_DATA', ttl=0)
