889ca87c8c166925f83c3098a0c9ffcb046bee55
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 14:57:05 -0400

    Update race_ethnicity.py

--- demographics/race_ethnicity.py
+++ demographics/race_ethnicity.py
@@ -1,3 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('Race and Ethnicity Trends')
+st.title("Race and Ethnicity Report:")
+st.subheader(f"{st.session_state['cbsa_selection']}")
