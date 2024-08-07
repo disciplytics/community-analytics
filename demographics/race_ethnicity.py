c5f4a9876e90f9dd22262c460f068d32a019e2c0
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 15:24:12 -0400

    Update race_ethnicity.py

--- demographics/race_ethnicity.py
+++ demographics/race_ethnicity.py
@@ -38,8 +38,8 @@ st.bar_chart(
   data = total_race_df[(total_race_df['Measure'].isin(race_filter)) & (total_race_df['FIVE_YEAR_ESTIMATE_DATE'] == total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max())].sort_values(by=['Counts'], ascending = True),
   x = 'Measure',
   y = 'Counts',
-  x_label = 'Total',
-  y_label = 'Race',
+  y_label = 'Total',
+  x_label = 'Race',
   #color = 'Measure',
   #stack = False,
-  horizontal=True,)
+  horizontal=False,)
