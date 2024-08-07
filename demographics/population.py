1e53c1d0b8342ed57de90078c16fef5e35a535df
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 12:42:31 -0400

    Update population.py

--- demographics/population.py
+++ demographics/population.py
@@ -9,7 +9,9 @@ st.title(f"Population Report: {st.session_state['cbsa_selection']}")
 conn = st.connection("snowflake")
 
 # total pop etl
-total_pop_sql = "SELECT GEO_NAME, VARIABLE_NAME as Race, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE VARIABLE_NAME = 'Race: Population | Total, 5yr Estimate' AND GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
+#total_pop_sql = "SELECT GEO_NAME, VARIABLE_NAME as Race, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE VARIABLE_NAME = 'Race: Population | Total, 5yr Estimate' AND GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
+total_pop_sql = "SELECT GEO_NAME, VARIABLE_NAME as Legend, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
+
 # get the total population
 total_pop_df = conn.query(total_pop_sql, ttl=0)
 
@@ -19,4 +21,5 @@ st.line_chart(
   x = 'FIVE_YEAR_ESTIMATE_DATE', 
   y = 'FIVE_YEAR_ESTIMATE',
   x_label = '5 Year Estimate Date',
-  y_label = 'Total Population Count')
+  y_label = 'Population Count',
+  color = 'Legend')
