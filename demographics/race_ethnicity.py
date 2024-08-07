82185d8f07dbddf6f8e61ea91a18dab180ad7856
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-07 14:58:42 -0400

    Update race_ethnicity.py

--- demographics/race_ethnicity.py
+++ demographics/race_ethnicity.py
@@ -4,3 +4,14 @@ import numpy as np
 
 st.title("Race and Ethnicity Report:")
 st.subheader(f"{st.session_state['cbsa_selection']}")
+
+
+# connect to snowflake
+conn = st.connection("snowflake")
+
+# race etl
+total_race_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE VARIABLE_NAME <> 'Race: Population | Total, 5yr Estimate' AND GEO_NAME = " + f"'{st.session_state['cbsa_selection']}'"
+# get the total race
+total_race_df = conn.query(total_race_sql, ttl=0)
+
+st.dataframe(total_race_df)
