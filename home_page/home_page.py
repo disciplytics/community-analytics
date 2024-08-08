4fcba5a0cbb41a41c3b9a42551bdef1ba558148e
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-08 13:08:47 -0400

    Update home_page.py

--- home_page/home_page.py
+++ home_page/home_page.py
@@ -16,9 +16,9 @@ cbsa_list = conn.query('SELECT GEO_NAME FROM CBSA_DATA_GEO_NAMES ORDER BY GEO_NA
 
 # return list of CBSA in a select box
 cbsa_selection = st.selectbox(
- 'Select a Metro/Micropolitan Area (Type a city name or state abbreviation to search)', cbsa_list)
+ 'Select a City (Type a city name or state abbreviation to search)', cbsa_list)
 
-st.write(f'Please enjoy the community report for the {cbsa_selection}. See the views on the left.')
+st.write(f'Please enjoy the community report for {cbsa_selection}. See the views on the left.')
 
 # get geojson for selected area
 geojson_sql = "SELECT COORDS FROM CBSA_DATA WHERE GEO_NAME = " + f"'{cbsa_selection}'" + "LIMIT 1"
