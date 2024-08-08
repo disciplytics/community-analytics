b86213e4f7da17760550d1d250847c97bf5d848a
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-08 16:35:53 -0400

    Update age.py

--- demographics/age.py
+++ demographics/age.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('Age Trends')
+st.title("Age Report:")
+st.subheader(f"{st.session_state['cbsa_selection']}")
+
+
+# connect to snowflake
+conn = st.connection("snowflake")
+
+# age and sex etl
+age_sex_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_RACE_DATA WHERE CITY_STATE = " + f"'{st.session_state['cbsa_selection']}'"
+age_sex_df = conn.query(age_sex_sql, ttl=0)
+
+st.dataframe(age_sex_df)
