9d085109eff95721004e2ccb6e0cf5579c29a67d
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-08 17:33:08 -0400

    Update age.py

--- demographics/age.py
+++ demographics/age.py
@@ -34,6 +34,30 @@ age_sex_df['FIVE_YEAR_ESTIMATE_DATE'] = pd.to_datetime(age_sex_df['FIVE_YEAR_EST
 age_sex_df_report = pd.pivot_table(
   age_sex_df, index = 'Age Range', columns = 'FIVE_YEAR_ESTIMATE_DATE', values = 'FIVE_YEAR_ESTIMATE', aggfunc = 'sum').fillna(0)
 
+# rename year ranges
+mapping_dict = {
+  'Und': 'Under 5 Yrs Old',
+  '5 to 9': '5 to 9 Yrs Old',
+  '10 to 14': '10 to 14 Yrs Old',
+  '15 to 17': '15 to 17 Yrs Old',
+  '18 and 19': '18 to 19 Yrs Old',
+  '20': '20 Yrs Old',
+  '21': '21 Yrs Old',
+  '22 to 24': '22 to 24 Yrs Old',
+  '25 to 29': '25 to 29 Yrs Old',
+  '30 to 34': '30 to 34 Yrs Old',
+  '35 to 39': '35 to 39 Yrs Old',
+  '40 to 44': '40 to 44 Yrs Old',
+  '45 to 49': '45 to 49 Yrs Old',
+  '50 to 54': '50 to 54 Yrs Old',
+  '55 to 59': '55 to 59 Yrs Old',
+  '60 and 61': '60 to 61 Yrs Old',
+  '62 to 64': '62 to 64 Yrs Old',
+  '65 to 66': '65 to 66 Yrs Old',
+  '67 to 69': '67 to 69 Yrs Old',
+  '70 to 74': '70 to 74 Yrs Old',
+  
+}
 st.dataframe(age_sex_df_report, use_container_width=True)
 
 st.write(age_sex_df['Age Range'].unique())
