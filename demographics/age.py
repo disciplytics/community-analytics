e0593f341b27b4c921b5b13f45768e141b3d40f1
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-09 08:54:58 -0400

    Update age.py

--- demographics/age.py
+++ demographics/age.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
+# Initialization
+if 'cbsa_selection' not in st.session_state:
+    st.session_state['cbsa_selection'] = 'Abbeville, Alabama'
+
 st.title("Age Report:")
 st.subheader(f"{st.session_state['cbsa_selection']}")
 
