c901fa79c21115d623f6db80591fc5f39ae8d22a
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-08 22:58:06 -0400

    Update race_ethnicity.py

--- demographics/race_ethnicity.py
+++ demographics/race_ethnicity.py
@@ -16,35 +16,48 @@ total_race_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Dat
 total_race_df = conn.query(total_race_sql, ttl=0)
 
 # prep data
-total_race_df['Measure'] = total_race_df['VARIABLE_NAME'].str.lstrip('Race: Population | ')
+race_dict = {
+  'Origin By Race: Population | Not Hispanic or Latino | White alone | Hispanic or Latino, 5yr Estimate': 'White', 
+  'Origin By Race: Population | Hispanic or Latino | Hispanic or Latino, 5yr Estimate': 'Hispanic or Latino',
+  'Origin By Race: Population | Not Hispanic or Latino | Some other race alone | Hispanic or Latino, 5yr Estimate': 'Other Race',
+  'Origin By Race: Population | Not Hispanic or Latino | Native Hawaiian and Other Pacific Islander alone | Hispanic or Latino, 5yr Estimate': 'Native Hawaiian and Other Pacific Islander',
+  'Origin By Race: Population | Not Hispanic or Latino | Black or African American alone | Hispanic or Latino, 5yr Estimate': 'Black or African American',
+  'Origin By Race: Population | Not Hispanic or Latino | American Indian and Alaska Native alone | Hispanic or Latino, 5yr Estimate': 'American Indian and Alaska Native',
+  'Origin By Race: Population | Not Hispanic or Latino | Asian alone | Hispanic or Latino, 5yr Estimate': 'Asian',
+  'Origin By Race: Population | Not Hispanic or Latino | Two or more races | Hispanic or Latino, 5yr Estimate':'Two or More Races'
+}
+total_race_df['Race'] = total_race_df['VARIABLE_NAME'].copy()
+
+total_race_df = total_race_df.replace({'Race', race_dict})
+
 total_race_df['FIVE_YEAR_ESTIMATE_DATE'] = pd.to_datetime(total_race_df['FIVE_YEAR_ESTIMATE_DATE']).dt.year.astype(int)
 total_race_df['Counts'] = total_race_df['FIVE_YEAR_ESTIMATE'].astype(int)
 
-race_filter = st.multiselect('Filter Races Here', total_race_df['Measure'].unique(),total_race_df['Measure'].unique())
+race_filter = st.multiselect('Filter Races Here', total_race_df['Race'].unique(),total_race_df['Race'].unique())
 st.write('Race Trends')
   
 st.bar_chart(
-    data = total_race_df[total_race_df['Measure'].isin(race_filter)],
+    data = total_race_df[total_race_df['Race'].isin(race_filter)],
     x = 'FIVE_YEAR_ESTIMATE_DATE',
     y = 'Counts',
     x_label = '5 Year Estimate Date',
     y_label = 'Total',
-    color = 'Measure',
+    color = 'Race',
     stack = False,
     use_container_width = True)
   
 st.write(f"Race Breakdown For Year {total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max()}")
   
 st.bar_chart(
-    data = total_race_df[(total_race_df['Measure'].isin(race_filter)) & (total_race_df['FIVE_YEAR_ESTIMATE_DATE'] == total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max())].sort_values(by=['Counts'], ascending = True),
-    x = 'Measure',
+    data = total_race_df[(total_race_df['Race'].isin(race_filter)) & (total_race_df['FIVE_YEAR_ESTIMATE_DATE'] == total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max())].sort_values(by=['Counts'], ascending = True),
+    x = 'Race',
     y = 'Counts',
     y_label = 'Total',
     x_label = 'Race',
-    #color = 'Measure',
+    #color = 'Race',
     #stack = False,
     horizontal=False,)
 
 st.write(f"Race Breakdown Table For Year {total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max()}")
-st.dataframe(total_race_df[(total_race_df['Measure'].isin(race_filter)) & (total_race_df['FIVE_YEAR_ESTIMATE_DATE'] == total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max())][['Measure', 'Counts']].sort_values(by=['Counts'], ascending = False).set_index(['Measure']),
+st.dataframe(total_race_df[(total_race_df['Race'].isin(race_filter)) & (total_race_df['FIVE_YEAR_ESTIMATE_DATE'] == total_race_df['FIVE_YEAR_ESTIMATE_DATE'].max())][['Race', 'Counts']].sort_values(by=['Counts'], ascending = False).set_index(['Measure']),
             use_container_width = True)
