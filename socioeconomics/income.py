bfff8925aef297dfe5e908b7bd1a4afe731d6d09
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-09 10:17:32 -0400

    Update income.py

--- socioeconomics/income.py
+++ socioeconomics/income.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.title('Income Trends')
+# Initialization
+if 'cbsa_selection' not in st.session_state:
+    st.session_state['cbsa_selection'] = 'Abbeville, Alabama'
+
+st.title("Income Report:")
+st.subheader(f"{st.session_state['cbsa_selection']}")
+
+
+# connect to snowflake
+conn = st.connection("snowflake")
+
+# income etl
+income_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_INCOME_DATA WHERE CITY_STATE = " + f"'{st.session_state['cbsa_selection']}'"
+income_df = conn.query(income_sql, ttl=0)
+
+st.dataframe(income_df)
