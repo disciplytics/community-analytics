3f366409ee9ba0ca7432485786701877b11997ed
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-09 10:41:17 -0400

    Update income.py

--- socioeconomics/income.py
+++ socioeconomics/income.py
@@ -15,15 +15,18 @@ conn = st.connection("snowflake")
 income_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_INCOME_DATA WHERE CITY_STATE = " + f"'{st.session_state['cbsa_selection']}'"
 income_df = conn.query(income_sql, ttl=0)
 
+# get the report year
+report_year = pd.to_datetime(income_df).dt.year.astype(int)
+
 # clean variables
 def var_cleaner(x):
     if x[:1] == 'F':
-        return x.lstrip('Family Income In The Past 12 Months (In 2022 Inflation-Adjusted Dollars): Population |').rstrip(', 5yr Estimate (2022)')
+        return x.lstrip(f'Family Income In The Past 12 Months (In {report_year} Inflation-Adjusted Dollars): Population |').rstrip(f'5yr Estimate ({report_year})')[:-1]
     elif x[:1] == 'H':
-        return x.lstrip('Household Income In The Past 12 Months (In 2022 Inflation-Adjusted Dollars): Population |').rstrip(', 5yr Estimate (2022)')
-    elif x == 'Median Family Income In The Past 12 Months (In 2022 Inflation-Adjusted Dollars): Population | Median family income in the past 12 months (in 2022 inflation-adjusted dollars), 5yr Estimate (2022)':
+        return x.lstrip(f'Household Income In The Past 12 Months (In {report_year} Inflation-Adjusted Dollars): Population |').rstrip(f'5yr Estimate ({report_year})')[:-1]
+    elif x == f'Median Family Income In The Past 12 Months (In {report_year} Inflation-Adjusted Dollars): Population | Median family income in the past 12 months (in {report_year} inflation-adjusted dollars), 5yr Estimate ({report_year})':
         return 'Median Family Income In The Past 12 Months'
-    elif x == 'Median Household Income In The Past 12 Months (In 2022 Inflation-Adjusted Dollars) By Household Size: Population | Total, 5yr Estimate (2022)':
+    elif x == f'Median Household Income In The Past 12 Months (In {report_year} Inflation-Adjusted Dollars) By Household Size: Population | Total, 5yr Estimate ({report_year})':
         return 'Median Household Income In The Past 12 Months'
         
 income_df['Measure'] = income_df['VARIABLE_NAME'].apply(var_cleaner)
