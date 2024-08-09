0a12dc24a04a5527cecb384aaf5e0e744a85c316
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-09 09:13:25 -0400

    Update age.py

--- demographics/age.py
+++ demographics/age.py
@@ -95,9 +95,6 @@ general_age_sex_df_report = general_age_sex_df_report.reindex([
   'Singles & Young Families', 'Familes & Empty Nesters', 'Enrichment Years Singles/Couples',
   'Retirement Opportunities'], level = 0)
 
-st.write('Phase of Life Table')
-st.dataframe(general_age_sex_df_report, use_container_width=True)
-
 # percent diff plot
 general_age_sex_df_pct_diff = general_age_sex_df[general_age_sex_df['FIVE_YEAR_ESTIMATE_DATE'] >= general_age_sex_df['FIVE_YEAR_ESTIMATE_DATE'].max()-1]
 
@@ -115,13 +112,85 @@ general_age_sex_df_pct_diff = general_age_sex_df_pct_diff.reindex([
   'Under 5 Yrs Old', '5 to 17 Yrs Old', '18 to 24 Yrs Old', '25 to 34 Yrs Old',
   '35 to 54 Yrs Old', '55 to 64 Yrs Old', '65 Yrs and Older']).reset_index()
 
-pctdiff_lc = alt.Chart(general_age_sex_df_pct_diff).mark_line().encode(
+
+## Children report
+
+children_mapping_dict = {
+  '5 to 9': '5 to 9 Yrs Old',
+  '10 to 14': '10 to 14 Yrs Old',
+  '15 to 17': '15 to 17 Yrs Old',
+}
+
+children_phase_mapping_dict = {
+  '5 to 9 Yrs Old': 'Early Elementary',
+  '10 to 14 Yrs Old': 'Late Elementary-Middle School',
+  '15 to 17 Yrs Old': 'High School',
+}
+
+# replace ages with readable values
+children_age_sex_df = age_sex_df.replace({'Age Range': children_mapping_dict})
+
+# create phase of life field
+children_age_sex_df['Phase of Life'] = children_age_sex_df['Age Range'].copy()
+children_age_sex_df = children_age_sex_df.replace({'Phase of Life': children_phase_mapping_dict})
+
+# make the children age table
+children_age_sex_df_report = pd.pivot_table(
+  children_age_sex_df, 
+  index = ['Phase of Life', 'Age Range'], 
+  columns = 'FIVE_YEAR_ESTIMATE_DATE', 
+  values = 'FIVE_YEAR_ESTIMATE', 
+  aggfunc = 'sum').fillna(0)
+
+# reorder indicies
+children_age_sex_df_report = children_age_sex_df_report.reindex([
+  'Early Elementary', 'Late Elementary-Middle School', 'High School'], level = 0)
+
+# percent diff plot
+children_age_sex_df_pct_diff = children_age_sex_df[children_age_sex_df['FIVE_YEAR_ESTIMATE_DATE'] >= children_age_sex_df['FIVE_YEAR_ESTIMATE_DATE'].max()-1]
+
+children_age_sex_df_pct_diff = children_age_sex_df_pct_diff.groupby(['Age Range', 'FIVE_YEAR_ESTIMATE_DATE'])['FIVE_YEAR_ESTIMATE'].sum().reset_index()
+
+children_age_sex_df_pct_diff = children_age_sex_df_pct_diff.sort_values(by = ['FIVE_YEAR_ESTIMATE_DATE'], ascending = True)
+
+children_age_sex_df_pct_diff['% Change In Last 2 Years'] = np.round(children_age_sex_df_pct_diff.sort_values('FIVE_YEAR_ESTIMATE_DATE').groupby(['Age Range']).FIVE_YEAR_ESTIMATE.pct_change(),2)
+
+children_age_sex_df_pct_diff = children_age_sex_df_pct_diff.dropna()
+
+# reindex
+children_age_sex_df_pct_diff = children_age_sex_df_pct_diff.set_index(['Age Range'])
+children_age_sex_df_pct_diff = children_age_sex_df_pct_diff.reindex([
+  '5 to 9 Yrs Old', '10 to 14 Yrs Old', '15 to 17 Yrs Old']).reset_index()
+
+
+age_tab, children_tab = st.tabs(['Age Overview', 'Children Overview'])
+
+with age_tab:
+
+    st.write('Phase of Life Table')
+    st.dataframe(general_age_sex_df_report, use_container_width=True)
+
+    
+    pctdiff_lc = alt.Chart(general_age_sex_df_pct_diff).mark_line().encode(
     x=alt.X('Age Range', sort = None),
     y=alt.Y('% Change In Last 2 Years').axis(format='%')
 )
 
-st.write('Percent Change in Population Over the Last Two Years')
-st.altair_chart(pctdiff_lc, use_container_width = True)
+    st.write('Percent Change in Population Over the Last Two Years')
+    st.altair_chart(pctdiff_lc, use_container_width = True)
+
+with children_tab:
+    st.write('Children Phase of Life Table')
+    st.dataframe(children_age_sex_df_report, use_container_width=True)
+
+    pctdiff_lc_child = alt.Chart(children_age_sex_df_pct_diff).mark_line().encode(
+        x=alt.X('Age Range', sort = None),
+        y=alt.Y('% Change In Last 2 Years').axis(format='%')
+    )
+
+    st.write('Percent Change in Child Population Over the Last Two Years')
+    st.altair_chart(pctdiff_lc_child, use_container_width = True)
+    
 
 
 
