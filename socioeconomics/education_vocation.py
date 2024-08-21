80f37700116c38e09564fa140a92138e05a842db
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-21 10:04:03 -0400

    Update education_vocation.py

--- socioeconomics/education_vocation.py
+++ socioeconomics/education_vocation.py
@@ -13,8 +13,27 @@ st.subheader(f"{st.session_state['cbsa_selection']}")
 # connect to snowflake
 conn = st.connection("snowflake")
 
-# education etl
-education_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_EDUCATION_DATA WHERE CITY_STATE = " + f"'{st.session_state['cbsa_selection']}'"
-education_df = conn.query(education_sql, ttl=0)
+edu_tab, voc_tab = st.tabs(['Educational Attainment', 'Vocations'])
 
-st.dataframe(education_df)
+
+with edu_tab:
+    st.write('Educational Attainment For The Population 25 to 64 Years Old')
+    # education etl
+    education_sql = "SELECT GEO_NAME, VARIABLE_NAME, DATE as Five_Year_Estimate_Date, VALUE as Five_Year_Estimate FROM CBSA_EDUCATION_DATA WHERE CITY_STATE = " + f"'{st.session_state['cbsa_selection']}'"
+    education_df = conn.query(education_sql, ttl=0)
+
+    def clean_edu(x):
+        if x == 'Educational Attainment By Employment Status For The Population 25 To 64 Years: Population | Some college or associate's degree, 5yr Estimate':
+            return 'Some College or Associate's Degree'
+        elif x == 'Educational Attainment By Employment Status For The Population 25 To 64 Years: Population | Less than high school graduate, 5yr Estimate':
+            return 'Less Than High School Graduate'
+        elif x == 'Educational Attainment By Employment Status For The Population 25 To 64 Years: Population | High school graduate (includes equivalency), 5yr Estimate':
+            return 'High School Graduate or Equivalent'
+        elif x == 'Educational Attainment By Employment Status For The Population 25 To 64 Years: Population | Bachelor's degree or higher, 5yr Estimate':
+            return "Bachelor's Degree of Higher"
+
+    # clean variables
+    education_df['Educational Attainment'] = education_df['VARIABLE_NAME'].apply(clean_edu)
+
+    
+    st.dataframe(education_df)
