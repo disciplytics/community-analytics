bd131ee0a2befb54d290d2d40d95e6ac291f86c9
disciplytics <122502050+disciplytics@users.noreply.github.com>
2024-08-21 14:27:19 -0400

    Update poverty.py

--- socioeconomics/poverty.py
+++ socioeconomics/poverty.py
@@ -158,11 +158,10 @@ with race_tab:
         st.altair_chart(total_lc2, use_container_width = True)
 
         st.write('Poverty Rate By Race')
-        race_lc2 = alt.Chart(race_poverty_df_race[race_poverty_df_race['Race'] != 'Total Poverty Rate']).mark_line().encode(
+        race_lc2 = alt.Chart(
+            race_poverty_df_race[race_poverty_df_race['Race'] != 'Total Poverty Rate']).mark_line().encode(
     x=alt.X('Year', sort = None),
     y=alt.Y('Rate').axis(format='%'),
     color='Race:N')
       
-    st.altair_chart(race_lc2, use_container_width = True)
-
-
+        st.altair_chart(race_lc2, use_container_width = True)
