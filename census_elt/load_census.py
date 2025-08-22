8a77ac6655389b7281a81fd51d355361ffec44a4
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-08-21 23:55:41 -0400

    Update load_census.py

--- census_elt/load_census.py
+++ census_elt/load_census.py
@@ -14,14 +14,33 @@ def get_racial_breakdown(state, year=2023, dataset="acs/acs5", api_key=None):
     variables_race = (
         "NAME",
         "DP05_0037PE", # White alone, percent
+        "DP05_0037M", # White alone, percent MoE
+        "DP05_0037PEA",
+        "DP05_0037PMA",
+        
         "DP05_0038PE", #Black or African American alone, percent
+        "DP05_0038M", #Black or African American alone, percent MoE
+        
         "DP05_0039PE", #American Indian and Alaska Native alone, percent
+        "DP05_0039M", #American Indian and Alaska Native alone, percent MoE
+        
         "DP05_0047PE", #Asian alone, percent    
+        "DP05_0047M", #Asian alone, percent MoE
+        
         "DP05_0055PE", #Native Hawaiian and Other Pacific Islander alone, percent
+        "DP05_0055M", #Native Hawaiian and Other Pacific Islander alone, percent MoE
+
         "DP05_0060PE", #Some other race alone, percent
+        "DP05_0060M", #Some other race alone, percent MoE
+        
         "DP05_0061PE", #Two or more races, percent
+        "DP05_0061M", #Two or more races, percent MoE
+        
         "DP05_0076PE", #Hispanic or Latino (of any race), percent
+        "DP05_0076M", #Hispanic or Latino (of any race), percent MoE
+        
         "DP05_0081PE"  #Not Hispanic or Latino, percent
+        "DP05_0081M"  #Not Hispanic or Latino, percent MoE
     )
 
     #df = pd.json_normalize(c.acs5st.state(variables_race, states.OH.fips, year=year))
