1dfe59a36987aa1848a12d81f1775c134d2bc23c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-08-21 22:13:47 -0400

    Update community_dashboard.py

--- pages/community_dashboard.py
+++ pages/community_dashboard.py
@@ -1,4 +1,12 @@
 import streamlit as st
 from census_elt.load_census import load_acs_data
 
-st.dataframe(load_acs_data(api_key=st.secrets["acs_key"]))
+st.dataframe(
+      load_acs_data(
+          year=2023, 
+          dataset="acs/acs5", 
+          variables=["NAME", "B01001_001E"], 
+          for_geo="zip code tabulation area:*", 
+          in_geo="state:39",
+          api_key=st.secrets["acs_key"]
+      )
